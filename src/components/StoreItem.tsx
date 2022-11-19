fa551f37cd6dfc2452d15ce26cbe1702852627a4
denizhankalkan <denizhankalkan@outlook.com>
2022-11-19 22:18:18 +0300

    Create add buttons for chart

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card } from "react-bootstrap";
+import {Card,Button } from "react-bootstrap";
 import formatCurrency from '../utilities/formatCurrency';
 
 type StoreItemProps = {
@@ -10,8 +10,9 @@ type StoreItemProps = {
 }
 
 function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
+    const quantity = 0
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img
             variant="top"
             src={imgUrl}
@@ -26,6 +27,18 @@ function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
                 <span className="ms-2 text-muted">{formatCurrency(price)}</span>
             </Card.Title>
             
+            <div className="mt-auto">
+             {quantity === 0 ? (
+              <Button className="w-100">+ Add To Cart</Button>
+             ): <div className="d-flex align-items-center" style={{gap: ".5rem"}}>
+                
+
+                <div className="d-flex align-items-center justify-content-center" style={{ gap: ".5rem"}}>
+                    <Button>-</Button>
+                    <Button>+</Button>
+                </div>
+                </div>}
+            </div>
         </Card.Body>
           
 
